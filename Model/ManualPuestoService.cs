43f5ceaa59a98c7ebd181625bd4701af73fc2f84
ANDRES CALVO VARGAS <andres.calvo.vargas@est.una.ac.cr>
2023-09-24 17:25:24 -0600

    Creación del proyecto de pruebas unitarias y de las clases necesarias en la carpeta MODEL para probarlas.

---
+++ Model/ManualPuestoService.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+
+namespace WebApplication1.Model
+{
+    public class ManualPuestoService
+    {
+        private string _manualPuesto;
+
+        public string ManualPuesto { get => _manualPuesto; set => _manualPuesto = value; }
+
+        public ManualPuestoService(string manualPuesto)
+        {
+            ManualPuesto = manualPuesto;
+        }
+
+        public ManualPuestoService()
+        {
+            ManualPuesto = " ";
+        }
+
+        public bool AccederManualPuesto()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = client.GetAsync(ManualPuesto).Result;
+
+                    return response.StatusCode == HttpStatusCode.OK;
+                }
+            }
+            catch (HttpRequestException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Model/SolicitudPuestoClaveService.cs
@@ -0,0 +1,35 @@
+﻿namespace WebApplication1.Model
+{
+    public class SolicitudPuestoClaveService
+    {
+        private string _NumPosiblesSucesores;
+
+        public string NumPosiblesSucesores { get => _NumPosiblesSucesores; set => _NumPosiblesSucesores = value; }
+
+        public SolicitudPuestoClaveService(string numPosiblesSucesores)
+        {
+            NumPosiblesSucesores = numPosiblesSucesores;
+        }
+
+        public SolicitudPuestoClaveService()
+        {
+            NumPosiblesSucesores = " ";
+        }
+
+        public bool SeleccionarCantidadPersonas(string cantidad)
+        {
+            switch (cantidad)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "más de 3":
+                    return true;
+                    break;
+                default:
+                    return false;
+                    break;
+            }
+        }
+    }
+}
