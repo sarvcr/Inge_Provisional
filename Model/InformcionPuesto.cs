a7a11f6b48f6711ad7ae8b3ed1756ae4a4d72d98
David R.A <daviramarc01@gmail.com>
2023-10-04 08:50:18 -0600

    Clases basicas: -FuncionesPuesto -FuncionarioActual -ResposabilidadesPuesto -GestionActivos -InformcionPuesto

---
+++ Model/InformcionPuesto.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Model
+{
+    public class InformcionPuesto
+    {
+        private string _nombre;
+        private string _dependencia;
+        private int _numeroPlaza;
+        private FuncionarioActual _funcionarioActual;
+        private List<FuncionesPuesto> _funcionesPuestos;
+        private Dictionary<string,string> _conocimientoEnAreas;
+        //private ManualDeProcedimiento _manualDeProcedimiento; probablemente esta clase ni se cree
+        private ResposabilidadesPuesto _resposabilidades;
+        private GestionActivos _activos;
+        private int[] _valorVariablesInfluyentes;
+
+    }
+}
---
+++ Model/ResposabilidadesPuesto.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Model
+{
+    public class ResposabilidadesPuesto
+    {
+        //private bool _
+    }
+}
